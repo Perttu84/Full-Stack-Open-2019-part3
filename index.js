371b28d6379257c28f43eb84ff0a54fe0985363e
Perttu Punakallio <perttu@Perttus-Air.home>
2019-07-03 23:00:57 +0300

    Finish exercise 3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ app.get('/api/persons/:id', (request, response) => {
 	}
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	persons = persons.filter(p => p.id !== id)
+
+	response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
   const numberOfPersons = persons.length
   const date = new Date()
