005181a3683624e02a8bc4a279ef0d4d3c5c0191
Perttu Punakallio <perttu@Perttus-Air.home>
2019-07-07 22:24:50 +0300

    Finish exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app= express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
@@ -9,41 +11,16 @@ app.use(express.static('build'))
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body) })
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    Person.find({}).then(persons => {
+  	response.json(persons.map(person => person.toJSON()))
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(p => p.id === id)
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	Person.findById(request.params.id).then(person => {
+		response.json(person.toJSON())
+	})
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -68,21 +45,14 @@ app.post('/api/persons', (request, response) => {
 		})
 	}
 
-   if (persons.find(p => p.name === body.name)) {
-   	   return response.status(400).json({
-   	   	error: "name must be unique"
-   	   })
-   }
-
-	const person = {
+	const person = new Person({
 		name: body.name,
 		number: body.number,
-		id: Math.floor(Math.random() * Math.floor(10000)),
-	}
-
-	persons = persons.concat(person)
-
-	response.json(person)
+	})
+    console.log(person)
+	person.save().then(savedPerson=> {
+		response.json(savedPerson.toJSON())
+	})
 })
 
 app.get('/info', (request, response) => {
@@ -93,7 +63,7 @@ app.get('/info', (request, response) => {
   	 <div>${date}</div>`)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
